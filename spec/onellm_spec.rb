554edbeac29c84eec26d901506afbf76ec81b047
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2025-01-01 21:40:30 -0800

    chore: remove useless test

--- spec/onellm_spec.rb
+++ spec/onellm_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Onellm do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Onellm::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
